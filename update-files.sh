adb48dfd314ba99576fb3b85e12c3ed79eed22e6
Mason Blalock <15042748+blalockma@users.noreply.github.com>
2022-09-17 21:00:59 -0400

    First iteration of update-files.sh to push file updates.

--- update-files.sh
+++ update-files.sh
@@ -1,2 +1,19 @@
-cp ./.zshrc ~
-cp ./.vimrc ~
+function update_if_new {
+	if [ -f $1 ]; then
+		cp $1 ~/$1
+		return
+	fi
+
+	hash_of_repo_file=$(sha1sum $1 | awk '{split($0,elements," "); print elements[1]}')
+	hash_of_existing_file=$(sha1sum ~/$1 | awk '{split($0,elements," "); print elements[1]}')
+
+	if [ $hash_of_repo_file != $hash_of_existing_file ]; then
+		cp $1 ~/$1
+	fi
+}
+
+root_rc_files=(".vimrc")
+
+for file in ${root_rc_files[@]}; do
+	update_if_new $file
+done
