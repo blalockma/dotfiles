c0dc16daf7f67a8f73c64440b1ebd357b1613cae
Mason Blalock <15042748+blalockma@users.noreply.github.com>
2022-09-17 22:17:26 -0400

    First iteration of init.sh setup

--- init.sh
+++ init.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
 
-# Replace .bashrc with skeleton version at /etc/skel/.bashrc
+cp /etc/skel/.bashrc ~
+rm -rf ~/bashrc_extensions
 
-# Update .bashrc to call extension rc files.
+script_directory=$(dirname $(realpath $0))
+cp -r $script_directory/configs/bashrc_extensions ~/bashrc_extensions
+
+shopt -s dotglob
+for file in $script_directory/configs/*; do
+	if [ ! -d "$file" ]; then
+		cp $file ~
+	fi
+done
+
+echo -e "\n\n# CUSTOM SECTION FROM git:masonblalock/dotfiles\n" >> ~/.bashrc
+echo "shopt -s dotglob" >> ~/.bashrc
+echo "for file in ~/bashrc_extensions/*; do" >> ~/.bashrc
+echo "	source \$file" >> ~/.bashrc
+echo "done" >> ~/.bashrc
 
 # Install oh-my-zsh
