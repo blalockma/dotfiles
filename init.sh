e0491cc8713a22a497ac4e34074e45301f7fdbdb
Mason Blalock <15042748+blalockma@users.noreply.github.com>
2022-09-17 23:21:51 -0400

    Change order and add git config

--- init.sh
+++ init.sh
@@ -6,13 +6,6 @@ script_directory=$(dirname $(realpath $0))
 rm -rf ~/bashrc_extensions
 cp -r $script_directory/configs/bashrc_extensions ~/bashrc_extensions
 
-shopt -s dotglob
-for file in $script_directory/configs/*; do
-	if [ ! -d "$file" ]; then
-		cp $file ~
-	fi
-done
-
 # Install zsh
 sudo apt update
 sudo apt install zsh
@@ -20,4 +13,15 @@ chsh -s $(which zsh)
 
 # Install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+shopt -s dotglob
+for file in $script_directory/configs/*; do
+	if [ ! -d "$file" ]; then
+		cp $file ~
+	fi
+done
+
 rm -f ~/.zshrc.pre-oh-my-zsh
+
+git config --global user.name "Mason Blalock"
+git config --global user.email "15042748+blalockma@users.noreply.github.com"
