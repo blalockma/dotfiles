5a59b7e2137fab65f70e7dd06a59130305d80812
Mason Blalock <15042748+blalockma@users.noreply.github.com>
2022-09-18 22:06:20 -0400

    Add azure CLI installation

--- init.sh
+++ init.sh
@@ -57,3 +57,6 @@ sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
 sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/microsoft-ubuntu-$(lsb_release -cs)-prod $(lsb_release -cs) main" > /etc/apt/sources.list.d/dotnetdev.list'
 sudo apt-get update
 sudo apt-get install azure-functions-core-tools-3
+
+# Install Azure CLI
+curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
