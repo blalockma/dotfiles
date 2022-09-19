01f2c4c137de34497a241ee92d8adcc7ab4bfcea
Mason Blalock <15042748+blalockma@users.noreply.github.com>
2022-09-19 02:39:58 -0400

    Add azure emulation and nvm/node

--- init.sh
+++ init.sh
@@ -70,3 +70,13 @@ sudo apt-get install silversearcher-ag
 
 # Install tmuxinator
 sudo apt-get install tmuxinator
+
+# Install nvm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+nvm install --lts
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+
+# Install azurite (azure emulation)
+npm install -g azurite
