b276d8bf58f2c962c6c4bb0934f0ff67876cb417
Mason Blalock <15042748+blalockma@users.noreply.github.com>
2025-03-11 04:33:41 +0000

    Add mason/lsp

--- nvim/.config/nvim/lua/masonblalock/plugins.lua
+++ nvim/.config/nvim/lua/masonblalock/plugins.lua
@@ -1,11 +1,18 @@
 return require('packer').startup(function()
     use 'wbthomason/packer.nvim'
+    use 'ThePrimeagen/harpoon'
+    use 'morhetz/gruvbox'
+    use 'nvim-telescope/telescope-file-browser.nvim'
+    use 'williamboman/mason.nvim'
+    use 'williamboman/mason-lspconfig.nvim'
+    use 'neovim/nvim-lspconfig'
+
     use {
         'nvim-telescope/telescope.nvim',
         requires = { {'nvim-lua/plenary.nvim'} }
     }
-    use {'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
-    use 'ThePrimeagen/harpoon'
-    use 'morhetz/gruvbox' 
-    use { "nvim-telescope/telescope-file-browser.nvim" }
+    use {
+        'nvim-telescope/telescope-fzf-native.nvim',
+        run = 'make'
+    }
 end)
